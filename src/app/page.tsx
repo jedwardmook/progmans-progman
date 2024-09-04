28705681cd8532910d2a37692bd4d172860ac7c6
JedwardMook <jedwardmook@yahoo.com>
2024-09-03 19:31:21 -0500

    starts initial idea of app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { useState } from "react";
+
+interface Todo {
+  text: string;
+  steps: Step[]
+  completed: boolean;
+}
+
+interface Step {
+  text: string;
+  completed: boolean
+}
 
 export default function Home() {
+  const [addTodoToggle, setAddTodoToggle] = useState<boolean>(false)
+  const [addStepToggle, setAddStepToggle] = useState<boolean>(false)
+  const [newTodo, setNewTodo] = useState<Todo>({
+    text: '',
+    steps: [],
+    completed: false,
+  })
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleNewTodoText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo({...newTodo, text: e.target.value});
+  }
+
+  const handleAddTodoClick = () => {
+    if (newTodo.text.trim() !== ''){
+      setTodos([...todos, newTodo]);
+      setNewTodo({ text: '', steps: [], completed: false });
+      setAddTodoToggle(!addTodoToggle)
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+      <h2>Todos:</h2>
+      {todos.map((todo, index) => {
+        return (
+          <div key={index}>
+            <span>{todo.text}</span>
+            {todo.steps.map((step, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                  />
+                  <span>{step.text}</span>
+                </div>
+              )
+            })}
+            <div>
+              {addStepToggle?
+                <input />
+                :
+                <button onClick={() => setAddStepToggle(!addStepToggle)}>Add Step</button>
+              }
+            </div>
+          </div>
+        )
+      })}
+      {addTodoToggle ? 
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <input
+            placeholder="Todo"
+            type="input"
+            value={newTodo.text}
+            onChange={handleNewTodoText}
+          />
+          <button onClick={handleAddTodoClick}>+</button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        :
+        <button onClick={() => setAddTodoToggle(!addTodoToggle)}>Add New</button>
+      }
     </main>
   );
 }
